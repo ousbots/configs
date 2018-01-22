607f971e4c1d8851032937ebc090f10d500e6a5b
Tim Ousborne <tousborne@gogoair.com>
2018-01-22 09:06:28 -0700

    Add fish alias and update config

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,9 @@
-set -x PATH $PATH /usr/sbin /usr/local/bin /usr/bin/core_perl
-
 # Base 16 Shell
 if status --is-interactive
     eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
 end
 
+# Keychain is loaded in bashrc now
 #if status --is-interactive
 #    keychain --env fish --quiet -Q id_rsa | source
 #end
