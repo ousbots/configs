de459e28bbbb8aa03aed816614b0fddf1f023ba3
tim <dev@sourbone.net>
2024-06-22 21:49:19 -0600

    [wezterm] move tab key change, turn on decorations, enable webgpu

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,10 @@
-local wezterm = require 'wezterm';
+local wezterm = require "wezterm";
 
 local mykeys = {}
 for i = 1, 8 do
   table.insert(mykeys, {
     key=tostring(i),
-    mods="CTRL",
+    mods="OPT",
     action=wezterm.action{MoveTab=i-1},
   })
 end
@@ -12,10 +12,13 @@ end
 return {
   font = wezterm.font("JetBrains Mono"),
   font_size = 16.0,
+  window_decorations = "TITLE | RESIZE",
   color_scheme = "catppuccin-mocha",
   colors = {
-    background = '#101110',
+    background = "#101110",
   },
   use_fancy_tab_bar = true,
+  adjust_window_size_when_changing_font_size = false,
   keys = mykeys,
+  front_end = "WebGpu",
 }
