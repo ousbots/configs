c9b18408c675cab319d0082a4a23427daa81892f
tim <dev@sourbone.com>
2024-09-19 09:47:00 -0600

    [fish] add go/bin to path, alias bat and fd, remove unused alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
 status is-interactive; and begin
     # Aliases
-    alias cl 'cd $argv and ls'
     alias l ls
     alias ll 'ls -a'
     alias ls 'eza -l --icons'
+    alias cat bat
+    alias find fd
 
     # Environment variables
     #set RIPGREP_CONFIG_PATH $HOME/.config/ripgreprc
@@ -12,6 +13,12 @@ status is-interactive; and begin
     if test -d $HOME/.cargo/bin
         contains $HOME/.cargo/bin $PATH
         or set PATH $HOME/.cargo/bin $PATH
+    end
+
+    # Go: $HOME/go/bin
+    if test -d $HOME/go/bin
+        contains $HOME/go/bin $PATH
+        or set PATH $HOME/go/bin $PATH
     end
 
     # Enable vi mode
