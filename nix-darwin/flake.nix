52d0170e629a100f7172a26f7f2a4b3200233368
tim <dev@sourbone.com>
2026-04-30 13:35:03 -0600

    [nix] build helix from git, package organization, package churn

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,43 +2,102 @@
   description = "Laptop flake";
 
   inputs = {
+    # NOTE: Use a specific commit with: nixpkgs.url = "github:NixOS/nixpkgs/91c9a64ce2a84e648d0cf9671274bb9c2fb9ba60";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    helix.url = "github:helix-editor/helix";
+    helix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, helix }:
   let
+
+    helixPackage = helix.packages.aarch64-darwin.default;
+
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # > nix-env -qaP | rg fish
+      # Packages installed in system profile
+      # NOTE: Attribute overriding example: (pkgs.nodejs_23.overrideAttrs (old: { version = "23.6.0"; }))
+      # NOTE: To search by name, run: nix-env -qaP | rg fish
       environment.systemPackages = [
-        # Attribute overriding example: [
-        # (pkgs.nodejs_23.overrideAttrs (old: { version = "23.6.0"; }))
-        # pkgs.angle-grinder
-        pkgs.asciinema
         pkgs.aspell
         pkgs.aspellDicts.en
         pkgs.aspellDicts.en-computers
         pkgs.aspellDicts.en-science
+        pkgs.gnupg
+        pkgs.jd-diff-patch
+
+        # Media tools
+        pkgs.asciinema
         pkgs.audacity
+        # pkgs.beets
+        # pkgs.chromaprint
+        pkgs.ffmpeg
+        pkgs.flac
+        pkgs.ghostscript
+        pkgs.graphviz
+        pkgs.imagemagick
+        pkgs.poppler
+        pkgs.poppler-utils
+        pkgs.rsgain
+        pkgs.transmission_4
+        # pkgs.yt-dlp
+
+        # SQL tools
+        # pkgs.pgcli
+        # pkgs.pspg
+        pkgs.sleek
+        # pkgs.sqlcheck
+        pkgs.sqlfluff
+        pkgs.sqruff
+
+        # AI tools
+        # pkgs.ccusage
+        pkgs.charm
+        pkgs.claude-code
+        pkgs.ollama
+
+        # Command-line tools
+        pkgs.angle-grinder
         pkgs.bat
-        #pkgs.colima
         pkgs.coreutils
-        pkgs.delta
-        pkgs.delve
-        pkgs.difftastic
-        #pkgs.docker-client
+        pkgs.dos2unix
         pkgs.duf
         pkgs.eza
         pkgs.fd
         pkgs.fish
-        pkgs.ffmpeg
-        pkgs.flow-control
-        # pkgs.ghostty
+        pkgs.fzf
+        pkgs.gdu
         pkgs.glow
-        pkgs.gnupg
-        pkgs.go_1_24
+        pkgs.htop
+        pkgs.lz4
+        pkgs.nvd
+        pkgs.rclone
+        pkgs.ripgrep
+        pkgs.sd
+        pkgs.starship
+        pkgs.unrar
+        pkgs.qsv
+        pkgs.yazi
+
+        # Development tools
+        helixPackage
+        pkgs.binutils
+        pkgs.delta
+        pkgs.difftastic
+        # pkgs.flow-control
+        pkgs.httpie
+        pkgs.jujutsu
+        pkgs.just
+        pkgs.k6
+        pkgs.mtr
+        pkgs.rsync
+        pkgs.tokei
+
+        # Go toolchain
+        pkgs.delve
+        pkgs.go
         pkgs.golangci-lint-langserver
         pkgs.golangci-lint
         pkgs.golines
@@ -47,34 +106,33 @@
         pkgs.gosec
         pkgs.govulncheck
         pkgs.go-tools
-        pkgs.graphviz
-        pkgs.helix
-        pkgs.htop
-        pkgs.httpie
-        pkgs.imagemagick
-        pkgs.jujutsu
-        pkgs.just
-        pkgs.k6
-        pkgs.mtr
+
+        # Python toolchain
+        pkgs.poetry
+        pkgs.python3
+        pkgs.uv
+        
+        # Rust toolchain
+        pkgs.cargo
+        pkgs.cargo-binutils
+        pkgs.clippy
+        pkgs.inferno
+        pkgs.rustc
+        pkgs.rustfmt
+        pkgs.rust-analyzer
+        # pkgs.arm-none-eabi-binutils
+
+        # Rust wasm tools
+        pkgs.binaryen
+        pkgs.wasm-bindgen-cli
+
+        # Typescript toolchain
         pkgs.nodejs_24
-        pkgs.nvd
-        pkgs.ollama
-        pkgs.pgcli
-        # pkgs.pspg
-        pkgs.ripgrep
-        pkgs.rustup
-        pkgs.rsync
-        pkgs.sd
-        pkgs.sleek
-        pkgs.sqlcheck
-        pkgs.sqlfluff
-        pkgs.sqruff
-        pkgs.starship
-        pkgs.swift-format
-        pkgs.tokei
+        pkgs.oxfmt
+        pkgs.oxlint
         pkgs.typescript-language-server
-        pkgs.unrar
-        pkgs.yt-dlp
+
+        # Zig development
         pkgs.zig
         pkgs.zoxide
       ];
@@ -85,10 +143,13 @@
       # Allow packages marked as broken.
       # nixpkgs.config.allowBroken = true;
 
-      # Necessary for using flakes on this system.
+      # Allow unsupported systems.
+      nixpkgs.config.allowUnsupportedSystem = true;
+
+      # Enable using flakes.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
+      # Enable fish shell support.
       programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
@@ -120,8 +181,7 @@
     };
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
+    # NOTE: Rebuild the darwin flake with: darwin-rebuild build --flake .#simple
     darwinConfigurations."timishi-laptop" = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
     };
